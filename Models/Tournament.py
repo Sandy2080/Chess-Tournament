ccaf74f9fc40ca239f652fb13bcdb3c4fbe31c3a
Sandy Ludosky <sludosky@xebia.fr>
2023-02-12 13:46:13 +0100

    create and save tournament

--- Models/Tournament.py
+++ Models/Tournament.py
@@ -1,25 +1,49 @@
+from tinydb import TinyDB
+
 class Tournament:
     """Tournoi d'échec."""
 
-    def __init__(self, name, location, starting_date, ending_date, rounds_total, players, current_round, description, rounds=4):
+    def __init__(self, name, location, starting_date, ending_date,  description, rounds=4):
         """Initialise les modalités du tournoi"""
+        self.tournament_id = 0
         self.name = name
         self.location = location
         self.starting_date = starting_date
         self.ending_date = ending_date
-        self.rounds_total = rounds_total
-        self.current_round = current_round
+        self.rounds_total = 0
+        self.current_round = 0
         self.players = []
         self.description = description
         self.rounds = rounds
+    
+    def save_to_db(self):
+        """Save new player to database """
+        tournaments_db = TinyDB('data/tournaments.json')
+        tournaments_db.all()
+        tournament_id = len(tournaments_db.all()) + 1
+        tournament_id = tournaments_db.insert({
+            "id": tournament_id,
+            "name": self.name,
+            "location": self.location,
+            "starting_date": self.starting_date,
+            "ending_date": self.ending_date,
+            "rounds_total": self.rounds_total, 
+            "rounds": self.rounds,
+            "current_round": self.current_round,
+            "players": self.players,
+            "description": self.description
+        })
 
     def describe(self):
+        tournaments_db = TinyDB('data/tournaments.json')
+        last_tournament = tournaments_db.all()[-1]
+        players = len(last_tournament["players"])
         print("\n\n----------------------------------")
-        print("***Tournament started :" + self.name.upper() + "***")
-        print("-Date: "+ str(self.starting_date))
-        print("-Location : "+ self.location)
-        print("-Rounds : "+ str(self.rounds))
-        print("-Current round : "+ str(self.current_round))
-        print("-Number of players : "+ str(len(self.players)))
+        print("***Tournament started :" +  last_tournament["name"].upper() + "***")
+        print("-Date: "+ last_tournament["starting_date"])
+        print("-Location : "+ last_tournament["location"])
+        print("-Rounds : "+ str(last_tournament["rounds"]))
+        print("-Current round : "+ str(last_tournament["current_round"]))
+        print("-Number of players : "+ str(players))
 
 
--- main.py
+++ main.py
@@ -1,9 +1,20 @@
 from Models.Player import Player
+from Models.Tournament import Tournament
+from datetime import date
+from time import gmtime, strftime
 
-def main():
+def create_player(): 
     player = Player("Sandy", "L", "DD/MM/YY", 0)
     player.save_to_db()
     print(player)
 
+def create_tournament(): 
+    tournament = Tournament("Le tournoi", "Paris",  str(date.today()), "", "")
+    tournament.save_to_db()
+    tournament.describe()
+
+def main():
+    pass
+    
 if __name__ == "__main__":
     main()
